acabca27e2096ecd142a8da72474cbb48f955409
Kousuke Nieda <nieda.kousuke@gmail.com>
2026-01-10 22:31:18 +0900

    TODO 新規作成 基本実装

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,6 +4,7 @@ import logoLight from "./logo-light.svg";
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+      hoge-
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
